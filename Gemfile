b0aff6bad529f72ddc0199f2c9ae248bbd8554d1
Levine <elevine@wva-172-31-148-166.mitre.org>
2012-08-23 15:55:17 -0400

    Setting up bootstrap less files.  Encountering a Less parser error with imports

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+gem "sinatra", :require => "sinatra/base"
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'less'
+gem "therubyracer", "~> 0.10.2"
+gem "thin"
---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+
+  assets do
+    css :bootstrap, [
+      '/css/bootstrap.css'
+    ]
+
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
